af61c3d08c1b66e63a923cea4bdee32f763d38ae
Vyacheslav Korotach <vkorotach@gmail.com>
2021-01-11 22:39:40 +0200

    Status added, participants changed

--- Accounts/iTMF_PoC_05/contracts/src/main/java/net/corda/samples/supplychain/states/ShippingRequestState.java
+++ Accounts/iTMF_PoC_05/contracts/src/main/java/net/corda/samples/supplychain/states/ShippingRequestState.java
@@ -1,11 +1,13 @@
 package net.corda.samples.supplychain.states;
 
+import net.corda.core.serialization.CordaSerializable;
 import net.corda.samples.supplychain.contracts.ShippingRequestStateContract;
 import net.corda.core.contracts.BelongsToContract;
 import net.corda.core.contracts.ContractState;
 import net.corda.core.identity.AbstractParty;
 import net.corda.core.identity.AnonymousParty;
 import net.corda.core.identity.Party;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,14 +23,22 @@ public class ShippingRequestState implements ContractState {
     private Party shippper;
     private String cargo;
     private List<AbstractParty> participants;
+    private Status status;
 
     public ShippingRequestState(AnonymousParty pickUpFrom, String deliverTo, Party shippper, String cargo) {
         this.pickUpFrom = pickUpFrom;
         this.deliverTo = deliverTo;
         this.shippper = shippper;
         this.cargo = cargo;
+        this.status = Status.GAME_IN_PROGRESS;
         this.participants = new ArrayList<AbstractParty>();
         participants.add(pickUpFrom);
+        participants.add(shippper);
+    }
+
+    @CordaSerializable
+    public enum Status {
+        GAME_IN_PROGRESS, GAME_OVER
     }
 
     public AnonymousParty getPickUpFrom() {
@@ -63,7 +73,7 @@ public class ShippingRequestState implements ContractState {
         this.cargo = cargo;
     }
 
-    @Override
+    @NotNull @Override
     public List<AbstractParty> getParticipants() {
         return this.participants;
     }
