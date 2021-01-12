e91efbc402ad612059576de4852f9e89b7e6b370
Vyacheslav Korotach <vkorotach@gmail.com>
2021-01-12 14:53:43 +0200

    SOPStateContract was changed

--- Accounts/iTMF_PoC_05/contracts/src/main/java/net/corda/samples/supplychain/contracts/SOPStateContract.java
+++ Accounts/iTMF_PoC_05/contracts/src/main/java/net/corda/samples/supplychain/contracts/SOPStateContract.java
@@ -9,7 +9,8 @@ import net.corda.core.transactions.LedgerTransaction;
 // ************
 public class SOPStateContract implements Contract {
     // This is used to identify our contract when building a transaction.
-    public static final String ID = "net.corda.samples.supplychain.contracts.CargoStateContract";
+//    public static final String ID = "net.corda.samples.supplychain.contracts.CargoStateContract";
+    public static final String ID = "net.corda.samples.supplychain.contracts.SOPStateContract";
 
     // A transaction is valid if the verify() function of the contract of all the transaction's input and output states
     // does not throw an exception.
