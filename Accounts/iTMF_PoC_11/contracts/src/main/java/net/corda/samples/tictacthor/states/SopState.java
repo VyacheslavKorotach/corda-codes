3148ddc178b52446b99076a95125f8465479b38b
Vyacheslav Korotach <vkorotach@gmail.com>
2021-01-16 16:21:15 +0200

    iTMF_PoC_11 added

---
+++ Accounts/iTMF_PoC_11/contracts/src/main/java/net/corda/samples/tictacthor/states/SopState.java
@@ -0,0 +1,175 @@
+package net.corda.samples.tictacthor.states;
+
+import net.corda.samples.tictacthor.contracts.SopContract;
+//import javafx.util.Pair;
+
+import kotlin.Pair;
+import net.corda.core.contracts.BelongsToContract;
+import net.corda.core.contracts.LinearState;
+import net.corda.core.contracts.UniqueIdentifier;
+import net.corda.core.identity.AbstractParty;
+import net.corda.core.identity.AnonymousParty;
+import net.corda.core.serialization.ConstructorForDeserialization;
+import net.corda.core.serialization.CordaSerializable;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.List;
+
+// *********
+// * State *
+// *********
+@BelongsToContract(SopContract.class)
+public class SopState implements LinearState {
+
+
+    private UniqueIdentifier paramedic;
+    private UniqueIdentifier patient;
+    private AnonymousParty me;
+    private AnonymousParty counterparty;
+    private boolean isPatientTurn;
+    private char[][] sopStep;
+    private UniqueIdentifier linearId;
+    private Status status;
+
+    public SopState(UniqueIdentifier paramedic, UniqueIdentifier patient, AnonymousParty me, AnonymousParty counterparty) {
+        //dynamic
+        this.paramedic = paramedic;
+        this.patient = patient;
+        this.me = me;
+        this.counterparty = counterparty;
+
+        //fixed
+        this.isPatientTurn = false;
+        this.sopStep = new char[][]{{'E','E','E'},{'E','E','E'},{'E','E','E'}};
+        this.linearId = new UniqueIdentifier();
+        this.status = Status.SOP_IN_PROGRESS;
+    }
+
+    @ConstructorForDeserialization
+    public SopState(UniqueIdentifier paramedic, UniqueIdentifier patient,
+                    AnonymousParty me, AnonymousParty counterparty,
+                    boolean isPatientTurn, UniqueIdentifier linearId,
+                    char[][] sopStep, Status status) {
+        this.paramedic = paramedic;
+        this.patient = patient;
+        this.me = me;
+        this.counterparty = counterparty;
+        this.isPatientTurn = isPatientTurn;
+        this.linearId = linearId;
+        this.sopStep = sopStep;
+        this.status = status;
+    }
+
+    @NotNull @Override
+    public UniqueIdentifier getLinearId() { return this.linearId; }
+
+    @NotNull @Override
+    public List<AbstractParty> getParticipants() { return Arrays.asList(me, counterparty); }
+
+
+    @CordaSerializable
+    public enum Status {
+        SOP_IN_PROGRESS, SOP_OVER
+    }
+
+    // Returns the party of the current player
+    public UniqueIdentifier getCurrentPlayerParty(){
+        if(isPatientTurn){
+            return patient;
+        }else{
+            return paramedic;
+        }
+    }
+
+    public char[][] deepCopy(){
+        char[][] newboard = new char[3][3];
+        for(int i = 0; i<this.sopStep.length; i++) {
+            for (int j = 0; j < this.sopStep[i].length; j++) {
+                newboard[i][j] = this.sopStep[i][j];
+            }
+        }
+        return newboard;
+    }
+
+    public SopState returnNewBoardAfterMove(Pair<Integer,Integer> pos, AnonymousParty me, AnonymousParty competitor){
+        if((pos.getFirst() > 2) ||(pos.getSecond()> 2)){
+            throw new IllegalStateException("Invalid board index.");
+        }
+        char[][] newborad = this.deepCopy();
+        if(isPatientTurn){
+            newborad[pos.getFirst()][pos.getSecond()] = 'X';
+        }else{
+            newborad[pos.getFirst()][pos.getSecond()] = 'O';
+        }
+        if(SopContract.SopUtils.isGameOver(newborad)){
+            SopState b = new SopState(this.paramedic,this.patient,me,competitor,!this.isPatientTurn,this.linearId, newborad,Status.SOP_OVER);
+            return b;
+        }else{
+            SopState b = new SopState(this.paramedic,this.patient,me,competitor,!this.isPatientTurn, this.linearId, newborad,Status.SOP_IN_PROGRESS);
+            return b;
+        }
+    }
+
+
+    //getter setter
+    public UniqueIdentifier getParamedic() {
+        return paramedic;
+    }
+
+    public UniqueIdentifier getPatient() {
+        return patient;
+    }
+
+    public AnonymousParty getMe() {
+        return me;
+    }
+
+    public AnonymousParty getCounterparty() {
+        return counterparty;
+    }
+
+    public boolean isPatientTurn() {
+        return isPatientTurn;
+    }
+
+    public char[][] getSopStep() {
+        return sopStep;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setParamedic(UniqueIdentifier paramedic) {
+        this.paramedic = paramedic;
+    }
+
+    public void setPatient(UniqueIdentifier patient) {
+        this.patient = patient;
+    }
+
+    public void setMe(AnonymousParty me) {
+        this.me = me;
+    }
+
+    public void setCounterparty(AnonymousParty counterparty) {
+        this.counterparty = counterparty;
+    }
+
+    public void setPatientTurn(boolean patientTurn) {
+        isPatientTurn = patientTurn;
+    }
+
+    public void setSopStep(char[][] sopStep) {
+        this.sopStep = sopStep;
+    }
+
+    public void setLinearId(UniqueIdentifier linearId) {
+        this.linearId = linearId;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+}
---
+++ Accounts/iTMF_PoC_11/workflows/src/main/java/net/corda/samples/tictacthor/flows/EndSopFlow.java
@@ -0,0 +1,156 @@
+package net.corda.samples.tictacthor.flows;
+
+import co.paralleluniverse.fibers.Suspendable;
+import com.r3.corda.lib.accounts.contracts.states.AccountInfo;
+import com.r3.corda.lib.accounts.workflows.services.AccountService;
+import com.r3.corda.lib.accounts.workflows.flows.RequestKeyForAccount;
+import com.r3.corda.lib.accounts.workflows.services.KeyManagementBackedAccountService;
+import com.sun.istack.NotNull;
+import net.corda.samples.tictacthor.accountUtilities.NewKeyForAccount;
+import net.corda.samples.tictacthor.contracts.SopContract;
+import net.corda.samples.tictacthor.states.SopState;
+import net.corda.core.contracts.StateAndRef;
+import net.corda.core.contracts.UniqueIdentifier;
+import net.corda.core.crypto.TransactionSignature;
+import net.corda.core.flows.*;
+import net.corda.core.identity.AnonymousParty;
+import net.corda.core.identity.Party;
+import net.corda.core.node.services.Vault;
+import net.corda.core.node.services.vault.QueryCriteria;
+import net.corda.core.transactions.SignedTransaction;
+import net.corda.core.transactions.TransactionBuilder;
+import net.corda.core.utilities.ProgressTracker;
+
+import java.security.PublicKey;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+// ******************
+// * Initiator flow *
+// ******************
+@InitiatingFlow
+@StartableByRPC
+public class EndSopFlow extends FlowLogic<String> {
+
+    private final ProgressTracker progressTracker = tracker();
+
+    private static final ProgressTracker.Step GENERATING_TRANSACTION = new ProgressTracker.Step("Generating a HeartState transaction");
+    private static final ProgressTracker.Step SIGNING_TRANSACTION = new ProgressTracker.Step("Signing transaction with our private key.");
+    private static final ProgressTracker.Step FINALISING_TRANSACTION = new ProgressTracker.Step("Recording transaction") {
+        @Override
+        public ProgressTracker childProgressTracker() {
+            return FinalityFlow.tracker();
+        }
+    };
+
+    private static ProgressTracker tracker() {
+        return new ProgressTracker(
+                GENERATING_TRANSACTION,
+                SIGNING_TRANSACTION,
+                FINALISING_TRANSACTION
+        );
+    }
+
+    @Override
+    public ProgressTracker getProgressTracker() {
+        return progressTracker;
+    }
+
+    //private variables
+    private String whoAmI ;
+    private String whereTo;
+    private UniqueIdentifier sopId;
+
+
+    //public constructor
+    public EndSopFlow(UniqueIdentifier sopId, String whoAmI, String whereTo){
+        this.sopId = sopId;
+        this.whoAmI = whoAmI;
+        this.whereTo = whereTo;
+    }
+
+    @Suspendable
+    @Override
+    public String call() throws FlowException {
+        //grab account service
+        AccountService accountService = getServiceHub().cordaService(KeyManagementBackedAccountService.class);
+        //grab the account information
+        AccountInfo myAccount = accountService.accountInfo(whoAmI).get(0).getState().getData();
+        PublicKey myKey = subFlow(new NewKeyForAccount(myAccount.getIdentifier().getId())).getOwningKey();
+
+        AccountInfo targetAccount = accountService.accountInfo(whereTo).get(0).getState().getData();
+        AnonymousParty targetAcctAnonymousParty = subFlow(new RequestKeyForAccount(targetAccount));
+
+        //retrieve the game board
+        QueryCriteria.LinearStateQueryCriteria queryCriteria = new QueryCriteria.LinearStateQueryCriteria()
+                .withUuid(Arrays.asList(sopId.getId())).withStatus(Vault.StateStatus.UNCONSUMED);
+        StateAndRef<SopState> inputBoardStateAndRef = getServiceHub().getVaultService().queryBy(SopState.class,queryCriteria).getStates().get(0);
+        if(inputBoardStateAndRef == null){
+            throw new IllegalArgumentException("You are in another game");
+        }
+
+        progressTracker.setCurrentStep(GENERATING_TRANSACTION);
+        //generating State for transfer
+
+        // Obtain a reference to a notary we wish to use.
+        /** METHOD 1: Take first notary on network, WARNING: use for test, non-prod environments, and single-notary networks only!*
+         *  METHOD 2: Explicit selection of notary by CordaX500Name - argument can by coded in flow or parsed from config (Preferred)
+         *
+         *  * - For production you always want to use Method 2 as it guarantees the expected notary is returned.
+         */
+        final Party notary = getServiceHub().getNetworkMapCache().getNotaryIdentities().get(0); // METHOD 1
+        // final Party notary = getServiceHub().getNetworkMapCache().getNotary(CordaX500Name.parse("O=Notary,L=London,C=GB")); // METHOD 2
+
+        TransactionBuilder txbuilder = new TransactionBuilder(notary)
+                .addInputState(inputBoardStateAndRef)
+                .addCommand(new SopContract.Commands.EndGame(),Arrays.asList(myKey,targetAcctAnonymousParty.getOwningKey()));
+
+
+        progressTracker.setCurrentStep(SIGNING_TRANSACTION);
+        //self verify and sign Transaction
+        txbuilder.verify(getServiceHub());
+        SignedTransaction locallySignedTx = getServiceHub().signInitialTransaction(txbuilder,Arrays.asList(getOurIdentity().getOwningKey(),myKey));
+
+        //Collect sigs
+        FlowSession sessionForAccountToSendTo = initiateFlow(targetAccount.getHost());
+        List<TransactionSignature> accountToMoveToSignature = (List<TransactionSignature>) subFlow(new CollectSignatureFlow(locallySignedTx,
+                sessionForAccountToSendTo,targetAcctAnonymousParty.getOwningKey()));
+        SignedTransaction signedByCounterParty = locallySignedTx.withAdditionalSignatures(accountToMoveToSignature);
+        progressTracker.setCurrentStep(FINALISING_TRANSACTION);
+        //Finalize
+        subFlow(new FinalityFlow(signedByCounterParty,
+                Arrays.asList(sessionForAccountToSendTo).stream().filter(it -> it.getCounterparty() != getOurIdentity()).collect(Collectors.toList())));
+
+        // We also distribute the transaction to the national regulator manually.
+//        subFlow(new ReportManually(signedByCounterParty, nationalRegulator));
+
+        return "SOP Completed";
+    }
+}
+
+
+@InitiatedBy(EndSopFlow.class)
+class EndSopFlowResponder extends FlowLogic<Void> {
+    //private variable
+    private FlowSession counterpartySession;
+
+    //Constructor
+    public EndSopFlowResponder(FlowSession counterpartySession) {
+        this.counterpartySession = counterpartySession;
+    }
+
+    @Override
+    @Suspendable
+    public Void call() throws FlowException {
+        subFlow(new SignTransactionFlow(counterpartySession) {
+            @Override
+            protected void checkTransaction(@NotNull SignedTransaction stx) throws FlowException {
+                // Custom Logic to validate transaction.
+            }
+        });
+        subFlow(new ReceiveFinalityFlow(counterpartySession));
+        return null;
+    }
+}
+
