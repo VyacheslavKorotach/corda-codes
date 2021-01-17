084cd5d59ff3b39273e0f70486c9c0975d85bb69
Vyacheslav Korotach <vkorotach@gmail.com>
2021-01-17 15:35:51 +0200

    EndFlow updated

--- Accounts/iTMF_PoC_11/contracts/src/main/java/net/corda/samples/tictacthor/contracts/SopContract.java
+++ Accounts/iTMF_PoC_11/contracts/src/main/java/net/corda/samples/tictacthor/contracts/SopContract.java
@@ -55,8 +55,9 @@ public class SopContract implements Contract {
             requireThat(require -> {
                 SopState input = (SopState) inputs.get(0);
                 SopState output = (SopState) outputs.get(0);
-                require.using("Input must have status SOP_IN_PROGRESS", output.getStatus() == SopState.Status.SOP_COMPLETED);
-                require.using("Next Sub Step of SOP either must be increment of previous one or be the Cancel Step", output.getSop() == 3 || output.getSop() == 4);
+                require.using("Input must have status SOP_COMPLETED", input.getStatus() == SopState.Status.SOP_COMPLETED);
+                require.using("Next Sub Step of SOP either must be increment of previous one or be the Cancel Step", input.getSop() == 3 || input.getSop() == 4);
+                require.using("Next Sub Step of SOP either must be increment of previous one or be the Cancel Step", input.getSop() == output.getSop());
                 return null;
             });
         }else{
