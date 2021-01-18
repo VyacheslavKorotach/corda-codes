d9a6388fe142916ada108e1402008c38787361b3
Vyacheslav Korotach <vkorotach@gmail.com>
2021-01-18 17:26:38 +0200

    Controller.java game to sop changed

--- Accounts/iTMF_PoC_11/clients/src/main/java/net/corda/samples/tictacthor/webserver/Controller.java
+++ Accounts/iTMF_PoC_11/clients/src/main/java/net/corda/samples/tictacthor/webserver/Controller.java
@@ -80,19 +80,6 @@ public class Controller {
         return myMap;
     }
 
-//    @PostMapping(value =  "createAccount/{acctName}")
-//    private ResponseEntity<String> createAccount(@PathVariable String acctName){
-//        try{
-//            String result = proxy.startTrackedFlowDynamic(CreateNewAccount.class,acctName).getReturnValue().get();
-//            return ResponseEntity.status(HttpStatus.CREATED).body("Account "+acctName+" Created");
-//
-//        }catch (Exception e) {
-//                return ResponseEntity
-//                        .status(HttpStatus.BAD_REQUEST)
-//                        .body(e.getMessage());
-//        }
-//    }
-
     @PostMapping(value =  "createNewAccount/{acctName}")
     private ResponseEntity<String> createNewAccount(@PathVariable String acctName){
         try{
@@ -105,22 +92,6 @@ public class Controller {
                     .body(e.getMessage());
         }
     }
-
-//    @PostMapping(value = "requestGameWith/{whoAmI}/{team}/{competeWith}")
-//    private ResponseEntity<String> requestGameWith(@PathVariable String whoAmI,@PathVariable String team, @PathVariable String competeWith){
-//        Set<Party> matchingPasties = proxy.partiesFromName(team,false);
-//        try{
-//
-//            Iterator iter = matchingPasties.iterator();
-//            String result = proxy.startTrackedFlowDynamic(ShareAccountTo.class,whoAmI,iter.next()).getReturnValue().get();
-//            return ResponseEntity.status(HttpStatus.CREATED).body("Game Request has Sent. When "+competeWith+" accepts your challenge, the game will start!");
-//
-//        }catch (Exception e) {
-//            return ResponseEntity
-//                    .status(HttpStatus.BAD_REQUEST)
-//                    .body(e.getMessage());
-//        }
-//    }
 
     @PostMapping(value = "ShareAccountTo/{whoAmI}/{Party}")
     private ResponseEntity<String> shareAccountTo(@PathVariable String whoAmI, @PathVariable String Party){
@@ -138,61 +109,6 @@ public class Controller {
         }
     }
 
-//    @PostMapping(value = "acceptGameInvite/{whoAmI}/{team}/{competeWith}")
-//    private ResponseEntity<String> acceptGameInvite(@PathVariable String whoAmI,@PathVariable String team, @PathVariable String competeWith){
-//        Set<Party> matchingPasties = proxy.partiesFromName(team,false);
-//        try{
-//
-//            Iterator iter = matchingPasties.iterator();
-//            String result = proxy.startTrackedFlowDynamic(ShareAccountTo.class,whoAmI,iter.next()).getReturnValue().get();
-//            return ResponseEntity.status(HttpStatus.CREATED).body("I, "+whoAmI+" accepts "+competeWith+"'s challenge. Let's play!");
-//
-//        }catch (Exception e) {
-//            return ResponseEntity
-//                    .status(HttpStatus.BAD_REQUEST)
-//                    .body(e.getMessage());
-//        }
-//    }
-
-//    @PostMapping(value = "startGameAndFirstMove/{whoAmI}/{competeWith}/{position}")
-//    private ResponseEntity<String> startGameAndFirstMove(@PathVariable String whoAmI,
-//                              @PathVariable String competeWith,
-//                              @PathVariable String position){
-//        int x  = -1;
-//        int y  = -1;
-//        int pos = Integer.parseInt(position);
-//        if(pos == 0) {
-//            x=0;y=0;
-//        }else if(pos == 1){
-//            x=0;y=1;
-//        }else if(pos == 2){
-//            x=0;y=2;
-//        }else if(pos == 3){
-//            x=1;y=0;
-//        }else if(pos == 4){
-//            x=1;y=1;
-//        }else if(pos == 5){
-//            x=1;y=2;
-//        }else if(pos == 6){
-//            x=2;y=0;
-//        }else if(pos == 7){
-//            x=2;y=1;
-//        }else if(pos == 8){
-//            x=2;y=2;
-//        }
-//        try{
-//            UniqueIdentifier gameId = proxy.startTrackedFlowDynamic(StartSopFlow.class,whoAmI,competeWith).getReturnValue().get();
-////            String submitTurn = proxy.startTrackedFlowDynamic(SubmitTurnFlow.class, gameId, whoAmI,competeWith,x,y).getReturnValue().get();
-//            String submitTurn = proxy.startTrackedFlowDynamic(SubmitTurnFlow.class, gameId, whoAmI,competeWith,pos).getReturnValue().get();
-//            return ResponseEntity.status(HttpStatus.CREATED).body("Game Id Created: "+gameId+", "+whoAmI+" made the first move on position ["+x+","+y+"].");
-//
-//        }catch (Exception e) {
-//            return ResponseEntity
-//                    .status(HttpStatus.BAD_REQUEST)
-//                    .body(e.getMessage());
-//        }
-//    }
-
     @PostMapping(value = "startSopFlow/{whoAmI}/{counterParty}")
     private ResponseEntity<String> startSopFlow(@PathVariable String whoAmI,
                                                 @PathVariable String counterParty){
@@ -209,62 +125,17 @@ public class Controller {
         }
     }
 
-
-//    @PostMapping(value = "submitMove/{whoAmI}/{competeWith}/{position}")
-//    private ResponseEntity<String> submitMove(@PathVariable String whoAmI,
-//                                                         @PathVariable String competeWith,
-//                                                         @PathVariable String position) {
-//        int x  = -1;
-//        int y  = -1;
-//        int pos = Integer.parseInt(position);
-//        if(pos == 0) {
-//            x=0;y=0;
-//        }else if(pos == 1){
-//            x=0;y=1;
-//        }else if(pos == 2){
-//            x=0;y=2;
-//        }else if(pos == 3){
-//            x=1;y=0;
-//        }else if(pos == 4){
-//            x=1;y=1;
-//        }else if(pos == 5){
-//            x=1;y=2;
-//        }else if(pos == 6){
-//            x=2;y=0;
-//        }else if(pos == 7){
-//            x=2;y=1;
-//        }else if(pos == 8){
-//            x=2;y=2;
-//        }
-//        try{
-//            UniqueIdentifier gameId = proxy.startTrackedFlowDynamic(mySop.class,whoAmI).getReturnValue().get().getLinearId();
-////            String submitTurn = proxy.startTrackedFlowDynamic(SubmitTurnFlow.class, gameId, whoAmI,competeWith,x,y).getReturnValue().get();
-//            String submitTurn = proxy.startTrackedFlowDynamic(SubmitTurnFlow.class, gameId, whoAmI,competeWith,pos).getReturnValue().get();
-//
-//            if(isGameOver(whoAmI)){
-//                proxy.startTrackedFlowDynamic(EndSopFlow.class, gameId, whoAmI,competeWith).getReturnValue().get();
-//                return ResponseEntity.status(HttpStatus.CREATED).body(""+whoAmI+" made the move on position ["+x+","+y+"], and Game Over");
-//            }else{
-//                return ResponseEntity.status(HttpStatus.CREATED).body(""+whoAmI+"+ made the move on position ["+x+","+y+"]");
-//            }
-//        }catch (Exception e) {
-//            return ResponseEntity
-//                    .status(HttpStatus.BAD_REQUEST)
-//                    .body(e.getMessage());
-//        }
-//    }
-
     @PostMapping(value = "SubmitTurnFlow/{whoAmI}/{counterParty}/{newSubStep}")
     private ResponseEntity<String> SubmitTurnFlow(@PathVariable String whoAmI,
                                                   @PathVariable String counterParty,
                                                   @PathVariable String newSubStep) {
         int sopSubStep = Integer.parseInt(newSubStep);
         try{
-            UniqueIdentifier gameId = proxy.startTrackedFlowDynamic(mySop.class,whoAmI).getReturnValue().get().getLinearId();
-            String submitTurn = proxy.startTrackedFlowDynamic(SubmitTurnFlow.class, gameId, whoAmI, counterParty,sopSubStep).getReturnValue().get();
+            UniqueIdentifier sopId = proxy.startTrackedFlowDynamic(mySop.class,whoAmI).getReturnValue().get().getLinearId();
+            String submitTurn = proxy.startTrackedFlowDynamic(SubmitTurnFlow.class, sopId, whoAmI, counterParty,sopSubStep).getReturnValue().get();
 
             if(isSopCompleted(whoAmI)){
-                proxy.startTrackedFlowDynamic(EndSopFlow.class, gameId, whoAmI, counterParty).getReturnValue().get();
+                proxy.startTrackedFlowDynamic(EndSopFlow.class, sopId, whoAmI, counterParty).getReturnValue().get();
                 return ResponseEntity.status(HttpStatus.CREATED).body(""+whoAmI+" changed the SOP sub step to "+sopSubStep+" and SOP was completed");
             }else{
                 return ResponseEntity.status(HttpStatus.CREATED).body(""+whoAmI+" changed the SOP sub step to "+sopSubStep);
@@ -283,11 +154,11 @@ public class Controller {
         int sopSubStep = 2;
         float tempVal = Float.parseFloat(temperatureValue);
         try{
-            UniqueIdentifier gameId = proxy.startTrackedFlowDynamic(mySop.class,whoAmI).getReturnValue().get().getLinearId();
-            String submitTurn = proxy.startTrackedFlowDynamic(SubmitTemperatureFlow.class, gameId, whoAmI, counterParty,sopSubStep, tempVal).getReturnValue().get();
+            UniqueIdentifier sopId = proxy.startTrackedFlowDynamic(mySop.class,whoAmI).getReturnValue().get().getLinearId();
+            String submitTurn = proxy.startTrackedFlowDynamic(SubmitTemperatureFlow.class, sopId, whoAmI, counterParty,sopSubStep, tempVal).getReturnValue().get();
 
             if(isSopCompleted(whoAmI)){
-                proxy.startTrackedFlowDynamic(EndSopFlow.class, gameId, whoAmI, counterParty).getReturnValue().get();
+                proxy.startTrackedFlowDynamic(EndSopFlow.class, sopId, whoAmI, counterParty).getReturnValue().get();
                 return ResponseEntity.status(HttpStatus.CREATED).body(""+whoAmI+" changed the SOP sub step to "+sopSubStep+" and SOP was completed");
             }else{
                 return ResponseEntity.status(HttpStatus.CREATED).body(""+whoAmI+" changed the SOP sub step to "+sopSubStep);
@@ -300,11 +171,11 @@ public class Controller {
     }
 
     private Boolean isSopCompleted(String whoAmI){
-        //If the game is over, the Status should be a null variable
-        //So if the status returned is GAME_IN_PROGRESS, it means the game is not over.
+        //If the SOP is over, the Status should be a null variable
+        //So if the status returned is SOP_IN_PROGRESS, it means the SOP is not over.
         try{
-            SopState.Status gameStatus = proxy.startTrackedFlowDynamic(mySop.class,whoAmI).getReturnValue().get().getStatus();
-            return gameStatus != SopState.Status.SOP_IN_PROGRESS;
+            SopState.Status sopStatus = proxy.startTrackedFlowDynamic(mySop.class,whoAmI).getReturnValue().get().getStatus();
+            return sopStatus != SopState.Status.SOP_IN_PROGRESS;
         }catch (Exception e) {
             throw new IllegalArgumentException("No SOP");
         }
